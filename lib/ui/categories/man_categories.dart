66bbe5348290610dcf77e824c9d233860625f1df
NurFortuna <aysenurfortuna@gmail.com>
2023-11-19 21:08:17 +0300

    added features

--- lib/ui/categories/man_categories.dart
+++ lib/ui/categories/man_categories.dart
@@ -5,175 +5,55 @@ class ManCategories extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     var screenSize = MediaQuery.of(context);
-    final double height = screenSize.size.height;
     final double width = screenSize.size.width;
 
-
     return Scaffold(
-      body: Column(
-        children: [
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            buildCategoryTile(context, "Tüm Ürünler", Icons.apps_outlined, width),
+            buildCategoryTile(context, "Giyim", Icons.checkroom_outlined, width),
+            buildCategoryTile(context, "Çanta", Icons.shopping_bag_rounded, width),
+            buildCategoryTile(context, "Aksesuar", Icons.diamond_outlined, width),
+            buildCategoryTile(context, "Ayakkabı", Icons.directions_run, width),
+            buildCategoryTile(context, "Kozmetik", Icons.scatter_plot, width),
+            buildCategoryTile(context, "Diğer", Icons.apps_outlined, width),
+          ],
+        ),
+      ),
+    );
+  }
 
-          Padding(
-            padding: const EdgeInsets.only(top: 15),
-            child: Container(
-              color: Colors.white,
-              width: width,
-              child: ListTile(
-                leading: Icon(Icons.apps_outlined),
-                title: Row(
-                  children: [
-                    Text("Tüm Ürünler",
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                      ),),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios,
-                      color: Colors.grey.shade600,),
-                  ],
+  Widget buildCategoryTile(BuildContext context, String title, IconData icon, double width) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 5),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pop(context, title);
+        },
+        child: Container(
+          color: Colors.white,
+          width: width,
+          child: ListTile(
+            leading: Icon(icon),
+            title: Row(
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.grey.shade600,
+                  ),
                 ),
-              ),
+                Spacer(),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.grey.shade600,
+                ),
+              ],
             ),
           ),
-
-          Padding(
-            padding: const EdgeInsets.only(top: 5),
-            child: Container(
-              color: Colors.white,
-              width: width,
-              child: ListTile(
-                leading: Icon(Icons.checkroom_outlined),
-                title: Row(
-                  children: [
-                    Text("Giyim",
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                      ),),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios,
-                      color: Colors.grey.shade600,),
-                  ],
-                ),
-              ),
-            ),
-          ),
-
-
-          Padding(
-            padding: const EdgeInsets.only(top: 5),
-            child: Container(
-              color: Colors.white,
-              width: width,
-              child: ListTile(
-                leading: Icon(Icons.shopping_bag_rounded),
-                title: Row(
-                  children: [
-                    Text("Çanta",
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                      ),),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios,
-                      color: Colors.grey.shade600,),
-                  ],
-                ),
-              ),
-            ),
-          ),
-
-
-          Padding(
-            padding: const EdgeInsets.only(top: 5),
-            child: Container(
-              color: Colors.white,
-              width: width,
-              child: ListTile(
-                leading: Icon(Icons.diamond_outlined),
-                title: Row(
-                  children: [
-                    Text("Aksesuar",
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                      ),),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios,
-                      color: Colors.grey.shade600,),
-                  ],
-                ),
-              ),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.only(top: 5),
-            child: Container(
-              color: Colors.white,
-              width: width,
-              child: ListTile(
-                leading: Icon(Icons.directions_run),
-                title: Row(
-                  children: [
-                    Text("Ayakkabı",
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                      ),),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios,
-                      color: Colors.grey.shade600,),
-                  ],
-                ),
-              ),
-            ),
-          ),
-
-
-          Padding(
-            padding: const EdgeInsets.only(top: 5),
-            child: Container(
-              color: Colors.white,
-              width: width,
-              child: ListTile(
-                leading: Icon(Icons.scatter_plot),
-                title: Row(
-                  children: [
-                    Text("Kozmetik",
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                      ),),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios,
-                      color: Colors.grey.shade600,),
-                  ],
-                ),
-              ),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.only(top: 5),
-            child: Container(
-              color: Colors.white,
-              width: width,
-              child: ListTile(
-                leading: Icon(Icons.apps_outlined),
-                title: Row(
-                  children: [
-                    Text("Diğer",
-                      style: TextStyle(
-                        color: Colors.grey.shade600,
-                      ),),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios,
-                      color: Colors.grey.shade600,),
-                  ],
-                ),
-              ),
-            ),
-          ),
-
-
-        ],
+        ),
       ),
     );
   }
